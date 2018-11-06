599651f9b5491ff8fa00d968f6a55a8c1170a50d
Meredith Green <44354558+aerhoespace@users.noreply.github.com>
2018-11-06 09:00:00 -0700

    Update bleh.c

--- bleh.c
+++ bleh.c
@@ -21,6 +21,9 @@
 
 // SmartMotorLibrary
 #include "jpearman/SmartMotorLib.c"
+// Other files
+#include "dcfunc.c"
+#include "autonfunc.c"
 
 #include "Vex_Competition_Includes.c"  // Main competition background code...do not modify!
 #pragma systemFile
@@ -108,96 +111,6 @@ task autonomous()
 /*  You must modify the code to add your own robot specific commands here.   */
 /*---------------------------------------------------------------------------*/
 
-// Sensors and driver control functions
-
-// Lift potentiometers
-int liftPotent(){
-	return(((SensorValue[leftPotent])*(SensorValue[rightPotent]))/2);
-}
-
-// Drive encoders
-int leftDriveEncode(){
-	return(SensorValue[leftEncoder]);
-}
-int rightDriveEncode(){
-	return(SensorValue[rightEncoder]);
-}
-// Drive function
-void driveFunc(int power1, int power2){
-	SetMotor(left1,power1);
-	SetMotor(right1,power2);
-	SetMotor(left2,power1);
-	SetMotor(right2,power2);
-	SetMotor(left3,power1);
-	SetMotor(right3,power2);
-}
-
-// User control drive functions
-void drive(){
-	driveFunc(vexRT[Ch2], vexRT[Ch3]);
-}
-
-// Lift function
-void liftFunc(int power){
-	SetMotor(leftlift,power);
-	SetMotor(rightlift,power);
-}
-
-// User control lift functions
-void liftUp(){
-	SetMotor(leftlift,100);
-	SetMotor(rightlift,100);
-}
-void liftDown(){
-	SetMotor(leftlift,-100);
-	SetMotor(rightlift,-100);
-}
-void liftStay(){
-	SetMotor(leftlift,0);
-	SetMotor(rightlift,0);
-}
-
-// User control flipper functions
-//void flipperUp(){
-//	SetMotor(flipper,100);
-//}
-//void flipperDown(){
-//	SetMotor(flipper,-50);
-//}
-//void flipperStay(){
-//	SetMotor(flipper,0);
-//}
-
-// User control claw functions
-//void clawOpen(){
-//	SetMotor(claw,25);
-//}
-//void clawClose(){
-//	SetMotor(claw,-50);
-//}
-//void clawStay(){
-//	SetMotor(claw,0);
-//}
-
-// User control intake functions
-void intakeUp(){
-	SetMotor(intake,100);
-}
-void intakeDown(){
-	SetMotor(intake,-100);
-}
-void intakeStay(){
-	SetMotor(intake,0);
-}
-
-// User control puncher functions
-void puncherShoot(){
-	SetMotor(puncher,80);
-}
-void puncherStay(){
-	SetMotor(puncher,0);
-}
-
 // User control code here, inside the loop
 
 // This is the main execution loop for the user control program.
@@ -218,13 +131,13 @@ task usercontrol()
 	}
 
 	// Lift program
-	if(vexRT[Btn6U]==1){
-		liftUp();
-		}else if (vexRT[Btn6D]==1){
-		liftDown();
-		}else {
-		liftStay();
-	}
+	//if(vexRT[Btn6U]==1){
+	//	liftUp();
+	//	}else if (vexRT[Btn6D]==1){
+	//	liftDown();
+	//	}else {
+	//	liftStay();
+	//}
 
 	// Flipper program
 	//if (vexRT[Btn8U]==1){
